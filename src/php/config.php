aac6a99d82989a0ea2e92ecbf3f5b98219fa7c9d
samh7 <85685871+samh7@users.noreply.github.com>
2024-03-27 21:43:45 +0300

    mc

--- src/php/config.php
+++ src/php/config.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect("db","root","","student_management_system") 
+$conn = mysqli_connect("localhost","root","","student_management_system") 
 or die("Could not connect to database");
 ;
